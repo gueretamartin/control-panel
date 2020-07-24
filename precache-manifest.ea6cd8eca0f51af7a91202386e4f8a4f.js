e4546cb98d5ea98553cd9899f1429397289f94e8
Martin Guereta <guereta.martin@gmail.com>
2020-07-24 20:43:06 -0300

    Updates

---
+++ precache-manifest.ea6cd8eca0f51af7a91202386e4f8a4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85144f41f5346ed72e315f54820ef3e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a5e693b4a1ed70603b3",
+    "url": "/static/css/2.7f5b6dec.chunk.css"
+  },
+  {
+    "revision": "e338cdd0e0d5a6cc7dad",
+    "url": "/static/css/main.1c40ba27.chunk.css"
+  },
+  {
+    "revision": "0a5e693b4a1ed70603b3",
+    "url": "/static/js/2.9a274ed1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9a274ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e338cdd0e0d5a6cc7dad",
+    "url": "/static/js/main.ffc971d4.chunk.js"
+  },
+  {
+    "revision": "67a2781c4ed8bfbbb24e",
+    "url": "/static/js/runtime-main.4e611aee.js"
+  }
+]);
