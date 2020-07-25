07679e9070b64b39ca7c345d3279c61456feec19
Martin Guereta <guereta.martin@gmail.com>
2020-07-24 21:06:31 -0300

    add BrowserRoute

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 import ThermometerRender from './components/ThermometerRender';  
 import Navbar from './components/Navbar'; 
 import "bootswatch/dist/darkly/bootstrap.min.css";
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 
 const routing = (
-  <Router>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+ <Router>
     <div>
       <Route path="/" component={Navbar} />
       <Route path="/" component={App} />
           <Route path="/thermometer" component={ThermometerRender} /> 
     </div>
   </Router>
+  </BrowserRouter>
 )
 ReactDOM.render(routing, document.getElementById('root'))
 
