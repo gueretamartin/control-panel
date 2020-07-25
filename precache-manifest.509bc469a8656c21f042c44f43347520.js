56165f4de2f57e41a4c60db5784da06ddb686b9a
Martin Guereta <guereta.martin@gmail.com>
2020-07-25 09:28:02 -0300

    Updates

--- precache-manifest.509bc469a8656c21f042c44f43347520.js
+++ precache-manifest.509bc469a8656c21f042c44f43347520.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d0d48d177093bac8b92bbd2be5049687",
+    "revision": "cdcaf5440fc3685a8af1cf3779e84905",
     "url": "/control-panel/index.html"
   },
   {
-    "revision": "e07101684c6ad7e1d22a",
+    "revision": "33ebbdad27b1b1ac12ea",
     "url": "/control-panel/static/css/2.1160d109.chunk.css"
   },
   {
-    "revision": "b8aba4d14a2d44f25506",
+    "revision": "28bc78871eb912ff865e",
     "url": "/control-panel/static/css/main.1c40ba27.chunk.css"
   },
   {
-    "revision": "e07101684c6ad7e1d22a",
-    "url": "/control-panel/static/js/2.6a49f60a.chunk.js"
+    "revision": "33ebbdad27b1b1ac12ea",
+    "url": "/control-panel/static/js/2.a3cb0c23.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/control-panel/static/js/2.6a49f60a.chunk.js.LICENSE.txt"
+    "url": "/control-panel/static/js/2.a3cb0c23.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b8aba4d14a2d44f25506",
-    "url": "/control-panel/static/js/main.4e4e297f.chunk.js"
+    "revision": "28bc78871eb912ff865e",
+    "url": "/control-panel/static/js/main.ebcb15a3.chunk.js"
   },
   {
     "revision": "2b5cab81e8a92fb0dd9c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/control-panel/precache-manifest.50f4eda14f87032c77922131e4b61036.js"
+  "/control-panel/precache-manifest.509bc469a8656c21f042c44f43347520.js"
 );
 
 self.addEventListener('message', (event) => {
